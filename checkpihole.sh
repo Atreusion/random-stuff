50dcd7b6ac06bd1c35ce1fdedf5ab97c763c5827
David <Atreusion@users.noreply.github.com>
2020-01-08 00:35:46 +0000

    Create checkpihole.sh

---
+++ checkpihole.sh
@@ -0,0 +1,11 @@
+piholeResult=$(pihole status 2>&1)
+
+if [ "$piholeResult" != "[✓] DNS service is running [✓] Pi-hole blocking is Enabled" ]; then
+    curl --retry 3 https://hc-ping.com/{healthcheckurl}
+    exit 1;
+else
+    curl --retry 3 https://hc-ping.com/{healthcheckurl}/fail
+    exit 1;
+fi
+
+# 0 5 * * * /home/pi/scripts/checkpihole.sh >/dev/null 2>&1
